6dacf917d29f3d232db7bbbd32c2355b74fba78c
v.bushuev <bushuev@2tbank.ru>
2014-10-29 18:14:45 +0300

    Кабинет

--- js/cover.js
+++ js/cover.js
@@ -1,5 +1,23 @@
+window.tym={};
+tym.view={};
+tym.view.Amount=(function(){
+	var tut=this;
+	var args=arguments.length?arguments[0]:"0";
+	tut.integer=null;
+	tut.fractional=null;
+	tut.toString=function(){
+		return tut.integer.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1&thinsp;')+"."+tut.fractional;
+	}
+	var arr=args.replace(/,/im,".").split(/\./);
+	tut.integer=arr[0];
+	tut.fractional=(arr.length>1)?arr[1]:0;
+});
 $(function(){
 	$(".merchant-category").click(function(){
 		$(this).find("a").click();
+	});
+	$(".amount").each(function(){
+		var amnt=new tym.view.Amount($(this).text());
+		$(this).html(amnt.toString()+'&nbsp;<a href="#" class="eye-hide"><i class="fa fa-eye-slash"></i></a>');
 	});
 });
